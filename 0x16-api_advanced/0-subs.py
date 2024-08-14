2582b0d5406d46e351e7e3b26af466f5947fa975
hd-David <mwewahoby@gmail.com>
2024-08-14 13:15:22 +0000

    how many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import requests 
+ 
+
+def number_of_subscribers(subreddit):
+    """ Returns a number of subscribersfor a given subreddit. 
+    Args:
+      subreddit: The name of the subreddit.
+
+    Returns:
+      The number of subscribers for the subreddit. If the subreddit is invalid, 
+      returns 0."""
+
+    headers = { 'User-Agent' : 'my-username/0.1'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        print(response.status_code)
+        
+        response.encoding = 'utf-8'
+        if response.status_code == 200:
+            data =response.json()
+            return data.get('data',{}).get('subscribers', 0)
+        elif response.status_code == 404:
+
+            return 0
+    except Exception as e:
+        print("Exceptions: {}".format(e))
+        return 0
