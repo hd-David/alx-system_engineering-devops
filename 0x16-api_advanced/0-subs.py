4ea464f5d5f71951d49c78569d3660843c7eee63
hd-David <mwewahoby@gmail.com>
2024-08-14 21:15:40 +0000

    Fixing subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,19 +12,21 @@ def number_of_subscribers(subreddit):
       The number of subscribers for the subreddit. If the subreddit is invalid, 
       returns 0."""
 
-    headers = { 'User-Agent' : 'my-username/0.1'}
+    headers = { "User-Agent" : "your-username/0.1 "}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         print(response.status_code)
         
+        # Set response encoding to utf-8 to handle special characters
         response.encoding = 'utf-8'
+
         if response.status_code == 200:
             data =response.json()
+            print(data)
             return data.get('data',{}).get('subscribers', 0)
         elif response.status_code == 404:
-
             return 0
     except Exception as e:
         print("Exceptions: {}".format(e))
